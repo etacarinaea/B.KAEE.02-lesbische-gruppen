630a01310065dda81e6304195dcf7fe569b9ac8b
Emily <etacarinaea@protonmail.com>
2022-01-21 14:59:14 +0100

    Move index to extra file

---
+++ data/index/places.js
@@ -0,0 +1,14 @@
+const indexPlaces = {
+  "asta": {
+    "desc": "Allgemeiner Studierendenausschuss (AStA)",
+    "coords": [51.54357860280159, 9.937440528630368]
+  },
+  "zhg": {
+    "desc": "Zentrales Hörsaalgebäude",
+    "coords": [51.54137440896165, 9.93569491281671]
+  },
+  "sub-neu": {
+    "desc": "Staats- und Universitätsbibliothek (Neubau)",
+    "coords": [51.53949026115184, 9.935934984512004]
+  }
+};
--- js/map-main.js
+++ js/map-main.js
@@ -1,19 +1,5 @@
 window.addEventListener('DOMContentLoaded', (event) => {
   const mapCenter = [51.533, 9.935];
-  const index = {
-    "asta": {
-      "desc": "Allgemeiner Studierendenausschuss (AStA)",
-      "coords": [51.54357860280159, 9.937440528630368]
-    },
-    "zhg": {
-      "desc": "Zentrales Hörsaalgebäude",
-      "coords": [51.54137440896165, 9.93569491281671]
-    },
-    "sub-neu": {
-      "desc": "Staats- und Universitätsbibliothek (Neubau)",
-      "coords": [51.53949026115184, 9.935934984512004]
-    }
-  };
 
   // Constants
   const bubbleSize = {
@@ -46,8 +32,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
   // Count all occurences of places first so we can create a scale
   let placeCounts = {};
-  for(let i = 0; i < Object.keys(index).length; ++i) {
-    let placeName = Object.keys(index)[i];
+  for(let i = 0; i < Object.keys(indexPlaces).length; ++i) {
+    let placeName = Object.keys(indexPlaces)[i];
     placeCounts[placeName] = 0
 
     for(let j = 0; j < csvArr.length; ++j) {
@@ -72,9 +58,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     if(count > 0) {
       let radius = ((count-placeMin)/(placeMax-placeMin))*(bubbleSize.max-bubbleSize.min) + bubbleSize.min;
-      L.circleMarker(index[placeName].coords, {...{radius: radius}, ...bubbleAttributes})
+      L.circleMarker(indexPlaces[placeName].coords, {...{radius: radius}, ...bubbleAttributes})
         .bindTooltip(
-          index[placeName].desc
+          indexPlaces[placeName].desc
           + "<hr>Erwähnungen: " + count.toString()
         ).addTo(map);
     }
