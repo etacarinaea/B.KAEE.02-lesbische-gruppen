bc19a00273a00ee24b0102936fe126481dd66ebb
Emily <etacarinaea@protonmail.com>
2022-01-28 09:34:38 +0100

    Update place index

--- data/index/places.js
+++ data/index/places.js
@@ -3,13 +3,13 @@ const indexPlaces = {
     "desc": "Allgemeiner Studierendenausschuss (AStA)",
     "coords": [51.54357860280159, 9.937440528630368]
   },
-  "asta-gos": {
-    "desc": "Allgemeiner Studierendenausschuss (AStA)",
-    "coords": [51.54357860280159, 9.937440528630368]
-  },
   "asta-nik": {
     "desc": "Allgemeiner Studierendenausschuss (AStA)<br>Altes Gebäude (1972-1988)",
     "coords": [51.538942098548006, 9.936226611917618]
+  },
+  "asta-gos": {
+    "desc": "Allgemeiner Studierendenausschuss (AStA)",
+    "coords": [51.54357860280159, 9.937440528630368]
   },
   "zhg": {
     "desc": "Zentrales Hörsaalgebäude",
@@ -30,6 +30,10 @@ const indexPlaces = {
   "gz": {
     "desc": "Grünes Zentrum",
     "coords": [51.533180, 9.939909]
+  },
+  "hc": {
+    "desc": "Historisches Colloquium",
+    "coords": [51.545085651766556, 9.945642548810262]
   },
   "hildesheim": {
     "desc": "Hildesheim",
@@ -59,4 +63,140 @@ const indexPlaces = {
     "desc": "Landesregierung Niedersachsen",
     "coords": [52.370556, 9.733611]
   },
+  "goe-tb": {
+    "desc": "Göttinger Tagesblatt",
+    "coords": [51.52895013967518, 9.930110182969834]
+  },
+  "wee-lst": {
+    "desc": "Weender Landstraße",
+    "coords": [51.54398084634991, 9.932132924853992]
+  },
+  "juzi": {
+    "desc": "Jugendzentrum Bürgerstraße 41",
+    "coords": [51.52920054589279, 9.933905724256874]
+  },
+  "theo": {
+    "desc": "Theologicum",
+    "coords": [51.54008402791116, 9.93464605682754]
+  },
+  "om10": {
+    "desc": "DGB-Haus <br> Seit 2009 leerstehend <br> Seit 2015 linkes Wohnprojekt",
+    "coords": [51.53610227496404, 9.931251454941055]
+  },
+  "bhf-fulda": {
+    "desc": "Bahnhof Fulda",
+    "coords": [50.55481829724356, 9.684256124997937]
+  },
+  "oec": {
+    "desc": "Oeconomicum",
+    "coords": [51.54097512439161, 9.93456431103249]
+  },
+  "roter-buchl": {
+    "desc": "Roter Buchladen",
+    "coords": [51.53309160444619, 9.937457050771396]
+  },
+  "mzg": {
+    "desc": "Mehrzweckgebäude - Blauer Turm",
+    "coords": [51.54226568336733, 9.935845119127226]
+  },
+  "t-keller": {
+    "desc": "Theaterkeller",
+    "coords": [51.52772017518522, 9.944003925700992]
+  },
+  "n-rhaus": {
+    "desc": "Neues Rathaus",
+    "coords": [51.5285357475087, 9.940397863646004]
+  },
+  "z-mensa": {
+    "desc": "Zentralmensa",
+    "coords": [51.5418048660299, 9.934528950313794]
+  },
+  "chelti": {
+    "desc": "Cheltenham Park",
+    "coords": [51.5319464809131, 9.9411507431637]
+  },
+  "stadthalle": {
+    "desc": "Stadthalle Göttingen",
+    "coords": [51.53405307091858, 9.942937074206347]
+  },
+  "ph": {
+    "desc": "Pädagogische Hochschule Göttingen",
+    "coords": [51.53444608971421, 9.946572681749585]
+  },
+  "markt": {
+    "desc": "Am Markt",
+    "coords": [51.53306541842703, 9.934590705568741]
+  },
+  "kbr14": {
+    "desc": "Kreuzbergring 14",
+    "coords": [51.5427977407022, 9.934642920285043]
+  },
+  "karlsruhe": {
+    "desc": "Karlsruhe",
+    "coords": [49.009428291416185, 8.402401567137659]
+  },
+  "sxsp": {
+    "desc": "Sex-Shop Jüdenstraße",
+    "coords": [51.533258901827836, 9.936829531019654]
+  },
+  "fuki-goe": {
+    "desc": "Film- und Kinoinitiative Göttingen",
+    "coords": [51.52756541400264, 9.94523392037561]
+  },
+  "frankfurt": {
+    "desc": "Frankfurt",
+    "coords": [50.11279028521461, 8.680488892832248]
+  },
+  "fz": {
+    "desc": "Frauenzentrum",
+    "coords": [51.53093444065935, 9.938355391288125]
+  },
+  "kl. cafe": {
+    "desc": "Kleines Café",
+    "coords": [51.53185208422115, 9.937992356241667]
+  },
+  "flt-kneipe": {
+    "desc": "Frauen- und Lesbenkneipe im Café Kabale",
+    "coords": [51.52763775154044, 9.944051341250336]
+  },
+  "kabale": {
+    "desc": "Café Kollektiv Kabale",
+    "coords": [51.52763775154044, 9.944051341250336]
+  },
+  // "rosa-v": {
+  //   "desc": "Rosa Villa",
+  //   "coords": []
+  // },
+  "luise": {
+    "desc": "Luisenschule",
+    "coords": [51.535915882753635, 9.943940189096535]
+  },
+  "lume": {
+    "desc": "Lumière Kino",
+    "coords": [51.52775052221437, 9.944108907698205]
+  },
+  "haus": {
+    "desc": "Besetztes Haus",
+    "coords": [51.52616977340218, 9.936694921464053]
+  },
+  // "f-haus": {
+  //   "desc": "Frauenhaus Göttingen e.V.",
+  //   "coords": []
+  // },
+  "ss": {
+    "desc": "Susanne Stemann <br> Psychologin für Frauen des Frauenhauses",
+    "coords": [51.531937775393, 9.938064729442369]
+  },
+  // "esg": {
+  //   "desc": "",
+  //   "coords": []
+  // },
+  "fr-z": {
+    "desc": "Frauenzentrum",
+    "coords": [51.533033285863965, 9.938864755717242]
+  },
+  // "": {
+  //   "desc": "",
+  //   "coords": []
+  // },
 };
